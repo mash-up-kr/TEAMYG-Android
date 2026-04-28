cda0b8b81b509973051f93a40cf2c8e677267e22
moon890309 <abcdefg8989@naver.com>
2026-04-29 07:13:27 +0900

    feat: MVI 인터페이스 및 User 관련 정의

---
+++ core/ui/src/main/java/com/tjyg/core/ui/mviContract.kt
@@ -0,0 +1,21 @@
+package com.tjyg.core.ui
+
+
+interface UiState
+interface UiIntent
+interface UiSideEffect
+
+data class UserState(
+    val isLoading: Boolean = false,
+    val userName: String = "",
+    val error: String? = null
+) : UiState
+
+sealed class UserIntent: UiIntent{
+    object LoadUser : UserIntent()
+    data class UpdateName(val name: String) : UserIntent()
+}
+
+sealed class UserSideEffect: UiSideEffect{
+    data class showToast(val message: String) : UserSideEffect()
+}
