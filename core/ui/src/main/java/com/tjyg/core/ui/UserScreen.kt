bae8ba163aa9498f2d77b9ea6138261e6d00e165
moon890309 <abcdefg8989@naver.com>
2026-04-29 07:17:51 +0900

    feat: UI작성 및 상태관찰

---
+++ core/ui/src/main/java/com/tjyg/core/ui/UserScreen.kt
@@ -0,0 +1,50 @@
+package com.tjyg.core.ui
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun UserScreen(viewModel: UserViewModel = viewModel()){
+    val state = viewModel.state.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit){
+        viewModel.effect.collect { effect ->
+            when(effect){
+                is UserSideEffect.showToast -> Toast.makeText(context, effect.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    if(state.value.isLoading){
+        Text(
+            modifier = Modifier.height(100.dp),
+            text = "로딩중입니다~"
+        )
+    }else{
+        Column{
+            Spacer(modifier = Modifier.height(100.dp))
+            Button(
+                onClick = {viewModel.processIntent(UserIntent.LoadUser)}){
+                Text(
+                    text = "불러오기",
+                )
+            }
+            Text(
+                modifier = Modifier.height(100.dp),
+                text = state.value.userName
+            )
+        }
+    }
+}
