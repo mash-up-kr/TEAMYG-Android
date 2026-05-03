e8f9900cd4236445c81eb29a6e1e51eb6cb88581
moon890309 <abcdefg8989@naver.com>
2026-05-03 23:15:44 +0900

    fix: MuatableSharedFlow -> SharedFlow 타입 변경

--- core/ui/src/main/java/com/tjyg/core/ui/BaseViewModel.kt
+++ core/ui/src/main/java/com/tjyg/core/ui/BaseViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -17,7 +18,7 @@ abstract class BaseViewModel<S : UiState, I : UiIntent, E : UiSideEffect>(
     val state = _state.asStateFlow()
 
     private val _effect = MutableSharedFlow<E>()
-    val effect: MutableSharedFlow<E> = _effect
+    val effect = _effect.asSharedFlow()
 
     abstract fun processIntent(intent: I)
 
