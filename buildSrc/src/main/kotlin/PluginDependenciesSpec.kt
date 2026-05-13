261936f399329d5055fc30cf1756a52abbd4e276
citytexi <heekyne66@gmail.com>
2026-05-13 20:06:55 +0900

    feat: buildSrc 제거

--- buildSrc/src/main/kotlin/PluginDependenciesSpec.kt
+++
@@ -1,5 +0,0 @@
-import org.gradle.plugin.use.PluginDependenciesSpec
-import org.gradle.plugin.use.PluginDependencySpec
-
-fun PluginDependenciesSpec.aliasYG(pluginName: String): PluginDependencySpec =
-    id("com.teamyg.plugin.$pluginName")
