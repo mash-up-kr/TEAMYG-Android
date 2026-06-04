4ecfab03bed71e4ca1df93fbebfbe6061d9baf05
citytexi <heekyne66@gmail.com>
2026-06-04 12:54:23 +0900

    feat: qualifier 및 logger tag 선언

---
+++ core/model/src/main/kotlin/com/teamyg/model/qualifier/CoreQualifier.kt
@@ -0,0 +1,7 @@
+package com.teamyg.model.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class CoreQualifier
---
+++ core/model/src/main/kotlin/com/teamyg/model/tag/LoggerTag.kt
@@ -0,0 +1,9 @@
+package com.teamyg.model.tag
+
+object LoggerTag {
+    const val FEATURE_TAG: String = "feature"
+    const val VIEWMODEL_TAG: String = "viewModel"
+    const val DOMAIN_TAG: String = "domain"
+    const val DATA_TAG: String = "data"
+    const val CORE_TAG: String = "core"
+}
