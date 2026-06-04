dcabe3c7a9431f856e5d380e0e09e1ecfc9ff01b
JeonK1 <kan5016@naver.com>
2026-06-04 20:59:47 +0900

    Delete : 불필요한 코드 제거

--- data/src/main/java/com/teamyg/data/repository/KakaoUserRepositoryImpl.kt
+++ data/src/main/java/com/teamyg/data/repository/KakaoUserRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.teamyg.data.repository
 
 import android.content.Context
-import android.util.Log
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
@@ -24,8 +23,6 @@ class KakaoUserRepositoryImpl @Inject constructor(
             context,
             callback = { token, error ->
                 if (error != null) {
-                    Log.e("LoginRoute", "카카오톡으로 로그인 실패", error)
-
                     // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
                     // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
                     if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
