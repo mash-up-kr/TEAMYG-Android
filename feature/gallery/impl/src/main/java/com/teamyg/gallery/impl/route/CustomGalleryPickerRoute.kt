a318232983bc7c124703065eaec5ad070b6f6e44
citytexi <heekyne66@gmail.com>
2026-06-05 13:27:17 +0900

    feat: 커스텀 갤러리 네비게이션 구조 추가

---
+++ feature/gallery/impl/src/main/java/com/teamyg/gallery/impl/route/CustomGalleryPickerRoute.kt
@@ -0,0 +1,13 @@
+package com.teamyg.gallery.impl.route
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.teamyg.navigation.Navigator
+
+@Composable
+internal fun CustomGalleryPickerRoute(
+    navigator: Navigator,
+    modifier: Modifier = Modifier,
+) {
+
+}
