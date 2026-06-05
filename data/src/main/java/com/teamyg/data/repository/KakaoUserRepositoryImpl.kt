a7da4498eb4d419552083a05d9b89a4fd6266dbb
JeonK1 <kan5016@naver.com>
2026-06-06 00:15:44 +0900

    chore : 중첩 if 문 when 문으로 정리

--- data/src/main/java/com/teamyg/data/repository/KakaoUserRepositoryImpl.kt
+++ data/src/main/java/com/teamyg/data/repository/KakaoUserRepositoryImpl.kt
@@ -24,30 +24,31 @@ class KakaoUserRepositoryImpl
             userApiClient.loginWithKakaoTalk(
                 context = context,
                 callback = { token, error ->
-                    if (error != null) {
-                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
-                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                    when {
+                        token != null -> {
+                            continuation.resume(
+                                value = KakaoLoginResult.Success(token = token.accessToken),
+                                onCancellation = null,
+                            )
+                        }
+                        error == null -> {
+                            continuation.resume(
+                                value = KakaoLoginResult.Failure(throwable = IllegalStateException("token is null")),
+                                onCancellation = null,
+                            )
+                        }
+                        error is ClientError && error.reason == ClientErrorCause.Cancelled -> {
                             continuation.resume(
                                 value = KakaoLoginResult.Cancel(throwable = error),
                                 onCancellation = null,
                             )
-                        } else {
+                        }
+                        else -> {
                             continuation.resume(
                                 value = KakaoLoginResult.Failure(throwable = error),
                                 onCancellation = null,
                             )
                         }
-                    } else if (token != null) {
-                        continuation.resume(
-                            value = KakaoLoginResult.Success(token = token.accessToken),
-                            onCancellation = null,
-                        )
-                    } else {
-                        continuation.resume(
-                            value = KakaoLoginResult.Failure(throwable = IllegalStateException("token is null")),
-                            onCancellation = null,
-                        )
                     }
                 },
             )
@@ -57,28 +58,31 @@ class KakaoUserRepositoryImpl
             userApiClient.loginWithKakaoAccount(
                 context = context,
                 callback = { token, error ->
-                    if (error != null) {
-                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                    when {
+                        token != null -> {
+                            continuation.resume(
+                                value = KakaoLoginResult.Success(token = token.accessToken),
+                                onCancellation = null,
+                            )
+                        }
+                        error == null -> {
+                            continuation.resume(
+                                value = KakaoLoginResult.Failure(throwable = IllegalStateException("token is null")),
+                                onCancellation = null,
+                            )
+                        }
+                        error is ClientError && error.reason == ClientErrorCause.Cancelled -> {
                             continuation.resume(
                                 value = KakaoLoginResult.Cancel(throwable = error),
                                 onCancellation = null,
                             )
-                        } else {
+                        }
+                        else -> {
                             continuation.resume(
                                 value = KakaoLoginResult.Failure(throwable = error),
                                 onCancellation = null,
                             )
                         }
-                    } else if (token != null) {
-                        continuation.resume(
-                            value = KakaoLoginResult.Success(token = token.accessToken),
-                            onCancellation = null,
-                        )
-                    } else {
-                        continuation.resume(
-                            value = KakaoLoginResult.Failure(throwable = IllegalStateException("token is null")),
-                            onCancellation = null,
-                        )
                     }
                 },
             )
