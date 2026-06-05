35b21eae2f788ab313cb61325df3c104c5a8b416
JeonK1 <kan5016@naver.com>
2026-06-06 00:32:03 +0900

    Feature : hilt-core -> javax-inject 로 라이브러리 전환

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
-    alias(libs.plugins.kotlin.ksp)
 }
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -13,6 +12,5 @@ kotlin {
     }
 }
 dependencies {
-    implementation(libs.hilt.core)
-    ksp(libs.hilt.compiler)
+    implementation(libs.javax.inject)
 }
