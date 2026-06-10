65c326ce2b3e3dd52d35c1191e2ac4795940492f
moon890309 <abcdefg8989@naver.com>
2026-06-10 20:51:09 +0900

    style: ktlint 수정

--- feature/canvas/impl/src/main/java/com/teamyg/canvas/impl/screen/CanvasEditScreen.kt
+++ feature/canvas/impl/src/main/java/com/teamyg/canvas/impl/screen/CanvasEditScreen.kt
@@ -3,7 +3,6 @@ package com.teamyg.canvas.impl.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
