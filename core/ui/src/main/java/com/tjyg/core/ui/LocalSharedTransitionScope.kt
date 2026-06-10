43dada32440a7fbdfc77bed9d60987b50ce73993
moon890309 <abcdefg8989@naver.com>
2026-06-10 20:41:04 +0900

    fix: 오류 문구 수정

--- core/ui/src/main/java/com/tjyg/core/ui/LocalSharedTransitionScope.kt
+++ core/ui/src/main/java/com/tjyg/core/ui/LocalSharedTransitionScope.kt
@@ -4,5 +4,5 @@ import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.runtime.compositionLocalOf
 
 val LocalSharedTransitionScope = compositionLocalOf<SharedTransitionScope> {
-    error("LocalSharedTransitionScope 오류!")
+    error("LocalSharedTransitionScope 초기값이 없습니다")
 }
