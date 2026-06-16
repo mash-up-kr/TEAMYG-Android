183f7824f00e8cc6f4b4e869d9203a809f2de20d
citytexi <heekyne66@gmail.com>
2026-06-16 09:04:53 +0900

    feat: 카카오 디자인 가이드 컬러 정의

---
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/theme/colors/KakaoDesignGuideColors.kt
@@ -0,0 +1,11 @@
+package com.teamyg.parfait.core.designsystem.theme.colors
+
+import androidx.compose.ui.graphics.Color
+
+object KakaoDesignGuideColors {
+    val SignContainerColor: Color = Color(0xFFFEE500)
+
+    val SignSymbolColor: Color = Color(0xFF000000)
+
+    val SignLabelColor: Color = Color(0xFF000000).copy(alpha = 0.85f)
+}
