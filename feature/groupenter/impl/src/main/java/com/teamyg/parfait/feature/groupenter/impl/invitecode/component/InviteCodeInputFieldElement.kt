3ea931b7bedd606c6b9ad26bda2f789e2269bcad
JeonK1 <kan5016@naver.com>
2026-06-20 00:41:58 +0900

    Feature : clickable 사용성 개선

--- feature/groupenter/impl/src/main/java/com/teamyg/parfait/feature/groupenter/impl/invitecode/component/InviteCodeInputFieldElement.kt
+++ feature/groupenter/impl/src/main/java/com/teamyg/parfait/feature/groupenter/impl/invitecode/component/InviteCodeInputFieldElement.kt
@@ -4,21 +4,28 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.teamyg.parfait.core.ui.preview.PreviewBox
 import com.teamyg.parfait.core.ui.preview.YGPreview
 
@@ -39,6 +46,7 @@ internal fun InviteCodeInputFieldElement(
     }
 
     Box(
+        contentAlignment = Alignment.Center,
         modifier = modifier
             .clip(RoundedCornerShape(4.dp))
             .background(color = Color(0x0F000000))
@@ -48,14 +56,24 @@ internal fun InviteCodeInputFieldElement(
                 } else {
                     Modifier
                 },
-            )
-            .clickable { onClickTextFieldElement() },
+            ),
     ) {
         BasicTextField(
             value = word,
+            textStyle = TextStyle(
+                color = Color(0xFF000000),
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+            ),
             cursorBrush = SolidColor(Color.Transparent),
             onValueChange = { value -> onValueChanged(value) },
             modifier = Modifier.focusRequester(focusRequester),
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { onClickTextFieldElement() },
         )
     }
 }
