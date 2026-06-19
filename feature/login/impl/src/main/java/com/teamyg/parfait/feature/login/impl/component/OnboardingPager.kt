da10eabe97de048c91beb7428989bee14e842ffe
citytexi <heekyne66@gmail.com>
2026-06-19 20:58:24 +0900

    refactor: box -> spacer

--- feature/login/impl/src/main/java/com/teamyg/parfait/feature/login/impl/component/OnboardingPager.kt
+++ feature/login/impl/src/main/java/com/teamyg/parfait/feature/login/impl/component/OnboardingPager.kt
@@ -135,7 +135,7 @@ private fun PagerIndicator(
                 label = "indicatorColor",
             )
 
-            Box(
+            Spacer(
                 modifier = Modifier
                     .size(dotSize)
                     .clip(CircleShape)
