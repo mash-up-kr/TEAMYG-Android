de1f76a396e1b77998c2c8d1375bb99ead6cc554
moon890309 <abcdefg8989@naver.com>
2026-06-21 15:58:22 +0900

    chore: MLKit 의존성 추가

--- feature/segmentation/impl/build.gradle.kts
+++ feature/segmentation/impl/build.gradle.kts
@@ -8,4 +8,6 @@ android {
 
 dependencies {
     implementation(projects.feature.segmentation.api)
+    implementation(libs.google.mlkit.subject.segmentation)
+    implementation(projects.feature.canvas.api)
 }
