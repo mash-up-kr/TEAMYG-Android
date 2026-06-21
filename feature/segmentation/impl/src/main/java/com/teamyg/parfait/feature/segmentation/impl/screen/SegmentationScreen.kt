a9b549f22bf139a4cca7d20dabd2767988fffbc3
moon890309 <abcdefg8989@naver.com>
2026-06-21 16:05:47 +0900

    feat: 이미지 변환 후 오버레이 이미지 구현

--- feature/segmentation/impl/src/main/java/com/teamyg/parfait/feature/segmentation/impl/screen/SegmentationScreen.kt
+++ feature/segmentation/impl/src/main/java/com/teamyg/parfait/feature/segmentation/impl/screen/SegmentationScreen.kt
@@ -6,23 +6,67 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.teamyg.parfait.core.ui.preview.PreviewBox
 import com.teamyg.parfait.core.ui.preview.YGPreview
+import com.teamyg.parfait.feature.segmentation.impl.viewmodel.SegmentationIntent
+import com.teamyg.parfait.feature.segmentation.impl.viewmodel.SegmentationViewModel
 
 @Composable
 internal fun SegmentationScreen(
     sourceImageUri: String,
     onClickBack: () -> Unit,
     modifier: Modifier = Modifier,
+    onClickOk: (file: String) -> Unit,
+    viewModel: SegmentationViewModel = hiltViewModel(),
 ) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceAround,
     ) {
-        Text(text = "누끼 화면 // sourceImageUri: $sourceImageUri")
+        Button(
+            onClick = { state.subjectImagePath?.let { onClickOk(it) } },
+            enabled = state.subjectImagePath != null,
+        ) {
+            Text("완료")
+        }
+
+        // 이미지 -> input image로 변경
+        LaunchedEffect(sourceImageUri) {
+            viewModel.processIntent(SegmentationIntent.LoadImage(sourceImageUri))
+        }
+
+        if (state.originBitmap != null) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                // 원본 이미지
+                Image(
+                    bitmap = state.originBitmap!!.asImageBitmap(),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                )
+                // 오버레이 이미지
+                state.overlayBitmap?.let {
+                    Image(
+                        bitmap = it.asImageBitmap(),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                    )
+                }
+            }
+        }
 
         Button(
             onClick = onClickBack,
@@ -39,5 +83,6 @@ private fun PreviewSegmentationScreen() = PreviewBox {
         sourceImageUri = "sourceImageUri",
         onClickBack = {},
         modifier = Modifier.fillMaxSize(),
+        onClickOk = {},
     )
 }
---
+++ feature/segmentation/impl/src/main/java/com/teamyg/parfait/feature/segmentation/impl/viewmodel/SegmentationViewModel.kt
@@ -0,0 +1,116 @@
+package com.teamyg.parfait.feature.segmentation.impl.viewmodel
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.segmentation.subject.SubjectSegmentation
+import com.google.mlkit.vision.segmentation.subject.SubjectSegmenterOptions
+import com.teamyg.parfait.core.ui.BaseViewModel
+import com.teamyg.parfait.core.ui.UiIntent
+import com.teamyg.parfait.core.ui.UiSideEffect
+import com.teamyg.parfait.core.ui.UiState
+import com.teamyg.parfait.feature.segmentation.impl.screen.decodeUriToBitmap
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+
+data class SegmentationState(
+    val originBitmap: Bitmap? = null,
+    val overlayBitmap: Bitmap? = null,
+    val subjectImagePath: String? = null,
+) : UiState
+
+sealed interface SegmentationIntent : UiIntent {
+    data class LoadImage(val imageUri: String?) : SegmentationIntent
+
+    object ChangeImage : SegmentationIntent
+}
+
+sealed interface SegmentationEffect : UiSideEffect
+
+@HiltViewModel
+class SegmentationViewModel
+@Inject constructor(
+    @ApplicationContext private val context: Context,
+) : BaseViewModel<SegmentationState, SegmentationIntent, SegmentationEffect>(
+    initialState = SegmentationState(),
+) {
+    private var inputImage: InputImage? = null // MLKit용 이미지
+
+    override fun processIntent(intent: SegmentationIntent) {
+        when (intent) {
+            is SegmentationIntent.LoadImage -> {
+                viewModelScope.launch {
+                    val bitmap = withContext(Dispatchers.IO) {
+                        decodeUriToBitmap(context.contentResolver, Uri.parse(intent.imageUri))
+                    }
+                    updateState { copy(originBitmap = bitmap) }
+                    inputImage = InputImage.fromBitmap(bitmap, 0)
+                    processIntent(SegmentationIntent.ChangeImage)
+                }
+            }
+
+            is SegmentationIntent.ChangeImage -> {
+                viewModelScope.launch {
+                    if (inputImage != null) {
+                        val image = inputImage!!
+                        // 전경 신뢰도 마스크
+                        val options = SubjectSegmenterOptions
+                            .Builder()
+                            .enableForegroundConfidenceMask()
+                            .build()
+                        // 주체 세그테이터 만들기
+                        val segmenter = SubjectSegmentation.getClient(options)
+                        // 이미지 처리
+                        segmenter
+                            .process(image)
+                            .addOnSuccessListener { subject ->
+                                // 분할 결과 가져오기
+                                val foregroundMask = subject.foregroundConfidenceMask!!
+                                val overlayColors = IntArray(image.width * image.height)
+                                val subjectColors = IntArray(image.width * image.height)
+
+                                for (i in 0 until image.width * image.height) {
+                                    if (foregroundMask[i] > 0.5f) {
+                                        overlayColors[i] = Color.argb(128, 255, 0, 255)
+                                        subjectColors[i] =
+                                            state.value.originBitmap!!.getPixel(i % image.width, i / image.width)
+                                    }
+                                }
+                                updateState {
+                                    copy(
+                                        overlayBitmap = Bitmap.createBitmap(
+                                            overlayColors,
+                                            image.width,
+                                            image.height,
+                                            Bitmap.Config.ARGB_8888,
+                                        ),
+                                    )
+                                }
+                                val subjectBitmap = Bitmap.createBitmap(
+                                    subjectColors,
+                                    image.width,
+                                    image.height,
+                                    Bitmap.Config.ARGB_8888,
+                                )
+
+                                val file = File(context.cacheDir, "subject.png")
+                                file.outputStream().use { subjectBitmap.compress(Bitmap.CompressFormat.PNG, 100, it) }
+                                updateState { copy(subjectImagePath = file.absolutePath) }
+                            }.addOnFailureListener { e ->
+                                Log.d("Segmentation", "error: $e")
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
