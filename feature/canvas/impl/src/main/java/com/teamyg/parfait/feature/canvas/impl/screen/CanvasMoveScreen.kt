c7f587165bec84219c7b937cc3689080a7368784
moon890309 <abcdefg8989@naver.com>
2026-06-21 16:06:49 +0900

    feat: 배경 제거 이미지 결과 확인

---
+++ feature/canvas/impl/src/main/java/com/teamyg/parfait/feature/canvas/impl/screen/CanvasMoveScreen.kt
@@ -0,0 +1,25 @@
+package com.teamyg.parfait.feature.canvas.impl.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import coil3.compose.AsyncImage
+
+@Composable
+internal fun CanvasMoveScreen(
+    modifier: Modifier = Modifier,
+    image: String,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        AsyncImage(
+            model = image,
+            contentDescription = null,
+        )
+    }
+}
