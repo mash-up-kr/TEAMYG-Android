ff32b9d50796ce69d1e4ffcb8331b41a5ac64916
citytexi <heekyne66@gmail.com>
2026-06-24 17:27:19 +0900

    refactor: mvi container 적용을 위한 로그인 화면 마이그레이션

--- feature/login/impl/src/main/java/com/teamyg/parfait/feature/login/impl/viewmodel/LoginViewModel.kt
+++ feature/login/impl/src/main/java/com/teamyg/parfait/feature/login/impl/viewmodel/LoginViewModel.kt
@@ -1,26 +1,19 @@
 package com.teamyg.parfait.feature.login.impl.viewmodel
 
 import android.util.Log
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.ViewModel
 import com.teamyg.parfait.domain.model.KakaoLoginResult
 import com.teamyg.parfait.domain.usecase.LoginWithKakaoUseCase
-import com.teamyg.parfait.core.ui.BaseViewModel
-import com.teamyg.parfait.core.ui.UiIntent
-import com.teamyg.parfait.core.ui.UiSideEffect
-import com.teamyg.parfait.core.ui.UiState
+import com.teamyg.parfait.core.ui.mvi.ContainerHost
+import com.teamyg.parfait.core.ui.mvi.extension.container
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class LoginState(
     val token: String? = null,
-) : UiState
+)
 
-sealed interface LoginIntent : UiIntent {
-    object LoginWithKakao : LoginIntent
-}
-
-sealed interface LoginSideEffect : UiSideEffect {
+sealed interface LoginSideEffect {
     class NavigateToNext : LoginSideEffect
 }
 
@@ -29,26 +22,24 @@ class LoginViewModel
 @Inject
 constructor(
     private val loginWithKakaoUseCase: LoginWithKakaoUseCase,
-) : BaseViewModel<LoginState, LoginIntent, LoginSideEffect>(initialState = LoginState()) {
-    override fun processIntent(intent: LoginIntent) {
-        when (intent) {
-            LoginIntent.LoginWithKakao -> {
-                viewModelScope.launch {
-                    when (val result = loginWithKakaoUseCase()) {
-                        is KakaoLoginResult.Success -> {
-                            updateState { copy(token = result.token) }
-                            Log.i(TAG, "카카오 계정으로 로그인 성공 : ${result.token}")
-                            postSideEffect(LoginSideEffect.NavigateToNext())
-                        }
+) : ViewModel(), ContainerHost<LoginState, LoginSideEffect> {
+    override val container = container<LoginState, LoginSideEffect>(
+        initialState = LoginState(),
+    )
 
-                        is KakaoLoginResult.Failure -> {
-                            Log.e(TAG, "카카오 계정으로 로그인 실패 : ${result.throwable}")
-                        }
-
-                        is KakaoLoginResult.Cancel -> Unit
-                    }
-                }
+    fun loginKakao() = intent {
+        when (val result = loginWithKakaoUseCase()) {
+            is KakaoLoginResult.Success -> {
+                reduce { copy(token = result.token) }
+                Log.i(TAG, "카카오 계정으로 로그인 성공 : ${result.token}")
+                postSideEffect(LoginSideEffect.NavigateToNext())
             }
+
+            is KakaoLoginResult.Failure -> {
+                Log.e(TAG, "카카오 계정으로 로그인 실패 : ${result.throwable}")
+            }
+
+            is KakaoLoginResult.Cancel -> Unit
         }
     }
 
