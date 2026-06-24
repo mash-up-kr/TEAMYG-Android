9cbc780a77d162e9243a0977f7f7aebab15017c6
citytexi <heekyne66@gmail.com>
2026-06-24 10:49:42 +0900

    feat: logger 디버그 로그라이터 삽입

--- app/src/main/java/com/teamyg/parfait/BaseApplication.kt
+++ app/src/main/java/com/teamyg/parfait/BaseApplication.kt
@@ -1,6 +1,8 @@
 package com.teamyg.parfait
 
 import android.app.Application
+import co.touchlab.kermit.Logger
+import co.touchlab.kermit.platformLogWriter
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -8,6 +10,11 @@ import dagger.hilt.android.HiltAndroidApp
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Logger.setLogWriters(listOf(platformLogWriter()))
+        }
+
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
 }
