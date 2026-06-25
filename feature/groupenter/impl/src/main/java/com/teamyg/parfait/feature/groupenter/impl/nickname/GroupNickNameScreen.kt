c6fdf07ea444c96d1bdd1b366886b9c6b69fdd4e
JeonK1 <kan5016@naver.com>
2026-06-26 01:00:42 +0900

    Feature : preview 코드 추가

--- feature/groupenter/impl/src/main/java/com/teamyg/parfait/feature/groupenter/impl/nickname/GroupNickNameScreen.kt
+++ feature/groupenter/impl/src/main/java/com/teamyg/parfait/feature/groupenter/impl/nickname/GroupNickNameScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -20,13 +21,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.teamyg.parfait.core.ui.preview.PreviewBox
+import com.teamyg.parfait.core.ui.preview.YGPreview
 import com.teamyg.parfait.feature.groupenter.impl.R
 
 @Composable
@@ -96,7 +100,7 @@ internal fun GroupNickNameScreen(
                         modifier = Modifier
                             .fillMaxWidth()
                             .border(width = 1.dp, color = Color(0xFFDFDFDF), shape = RoundedCornerShape(8.dp))
-                            .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 11.dp)
+                            .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 11.dp),
                     )
                 }
             }
@@ -118,4 +122,28 @@ internal fun GroupNickNameScreen(
                 .align(Alignment.BottomCenter),
         )
     }
+}
+
+private class GroupNickNameScreenPreviewParameterProvider :
+    PreviewParameterProvider<GroupNickNameUiState> {
+    override val values: Sequence<GroupNickNameUiState>
+        get() = sequenceOf(
+            GroupNickNameUiState(""),
+            GroupNickNameUiState(nickName = "he"),
+            GroupNickNameUiState(nickName = "hello"),
+        )
+}
+
+@YGPreview
+@Composable
+private fun GroupNickNameScreenPreview(
+    @PreviewParameter(GroupNickNameScreenPreviewParameterProvider::class) uiState: GroupNickNameUiState,
+) = PreviewBox {
+    GroupNickNameScreen(
+        uiState = uiState,
+        onValueChanged = { },
+        onClickNextButton = {},
+        onClickBackButton = {},
+        modifier = Modifier.fillMaxSize(),
+    )
 }
