6a71a2d620e1eb2ce65937a06a57116bab19fe87
citytexi <heekyne66@gmail.com>
2026-06-25 23:52:59 +0900

    refactor: logger import 수정

--- core/util/jvm/src/main/kotlin/com/teamyg/parfait/core/util/jvm/analytics/LoggerInitializer.kt
+++ core/util/jvm/src/main/kotlin/com/teamyg/parfait/core/util/jvm/analytics/LoggerInitializer.kt
@@ -1,11 +1,11 @@
 package com.teamyg.parfait.core.util.jvm.analytics
 
+import co.touchlab.kermit.Logger
 import co.touchlab.kermit.platformLogWriter
 
 object LoggerInitializer {
     fun setupDebug() {
-        co.touchlab.kermit.Logger
-            .setLogWriters(platformLogWriter())
+        Logger.setLogWriters(platformLogWriter())
     }
 
     fun setupRelease() {
