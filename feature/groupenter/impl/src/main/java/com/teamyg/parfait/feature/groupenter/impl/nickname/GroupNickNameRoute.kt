dc2ffa1e8e94b6798f61f6b4459d52f151d7ce4e
JeonK1 <kan5016@naver.com>
2026-06-26 00:58:14 +0900

    Feature : UI 로직 구현

--- feature/groupenter/impl/src/main/java/com/teamyg/parfait/feature/groupenter/impl/nickname/GroupNickNameRoute.kt
+++ feature/groupenter/impl/src/main/java/com/teamyg/parfait/feature/groupenter/impl/nickname/GroupNickNameRoute.kt
@@ -2,6 +2,7 @@ package com.teamyg.parfait.feature.groupenter.impl.nickname
 
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -16,8 +17,23 @@ fun GroupNickNameRoute(
 ) {
     val uiState by viewModel.state.collectAsStateWithLifecycle()
 
+    LaunchedEffect(Unit) {
+        viewModel.effect.collect { effect ->
+            when (effect) {
+                GroupNickNameSideEffect.NavigateToBack -> {
+                    navigator.onBack()
+                }
+
+                GroupNickNameSideEffect.NavigateToNext -> { /* navigate to next */ }
+            }
+        }
+    }
+
     GroupNickNameScreen(
         uiState = uiState,
+        onValueChanged = { word -> viewModel.processIntent(GroupNickNameIntent.InputWord(word)) },
+        onClickNextButton = { viewModel.processIntent(GroupNickNameIntent.ClickNextButton) },
+        onClickBackButton = { viewModel.processIntent(GroupNickNameIntent.ClickBackButton) },
         modifier = modifier.imePadding(),
     )
 }
