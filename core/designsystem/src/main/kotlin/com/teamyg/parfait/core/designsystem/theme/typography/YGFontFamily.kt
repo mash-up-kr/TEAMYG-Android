0eae79c900d1df9ced3c48b9d7f677e5787c2dff
citytexi <heekyne66@gmail.com>
2026-06-26 00:22:29 +0900

    refactor: 폰트 필요한거 제외한 나머지 제거

--- core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/theme/typography/YGFontFamily.kt
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/theme/typography/YGFontFamily.kt
@@ -7,14 +7,9 @@ import com.teamyg.parfait.core.designsystem.R
 
 object YGFontFamily {
     val suitFontFamily: FontFamily = FontFamily(
-        Font(R.font.suit_thin, FontWeight.Thin),
-        Font(R.font.suit_extra_light, FontWeight.ExtraLight),
-        Font(R.font.suit_light, FontWeight.Light),
         Font(R.font.suit_regular, FontWeight.Normal),
         Font(R.font.suit_medium, FontWeight.Medium),
         Font(R.font.suit_semi_bold, FontWeight.SemiBold),
         Font(R.font.suit_bold, FontWeight.Bold),
-        Font(R.font.suit_extra_bold, FontWeight.ExtraBold),
-        Font(R.font.suit_heavy, FontWeight.Black),
     )
 }
