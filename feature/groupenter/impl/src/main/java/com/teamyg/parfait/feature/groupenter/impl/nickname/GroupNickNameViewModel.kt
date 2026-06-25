778dd5e8cc4dfc5474bbc8570b446df4fcbf26a8
JeonK1 <kan5016@naver.com>
2026-06-26 00:58:14 +0900

    Feature : UI 로직 구현

--- feature/groupenter/impl/src/main/java/com/teamyg/parfait/feature/groupenter/impl/nickname/GroupNickNameViewModel.kt
+++ feature/groupenter/impl/src/main/java/com/teamyg/parfait/feature/groupenter/impl/nickname/GroupNickNameViewModel.kt
@@ -1,19 +1,28 @@
 package com.teamyg.parfait.feature.groupenter.impl.nickname
 
+import androidx.lifecycle.viewModelScope
 import com.teamyg.parfait.core.ui.BaseViewModel
 import com.teamyg.parfait.core.ui.UiIntent
 import com.teamyg.parfait.core.ui.UiSideEffect
 import com.teamyg.parfait.core.ui.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-data class GroupNickNameUiState(val dummy: String = "") : UiState {
-}
+data class GroupNickNameUiState(val nickName: String = "") : UiState
 
 sealed interface GroupNickNameIntent : UiIntent {
+    data object ClickNextButton : GroupNickNameIntent
+
+    data object ClickBackButton : GroupNickNameIntent
+
+    data class InputWord(val nickName: String) : GroupNickNameIntent
 }
 
 sealed interface GroupNickNameSideEffect : UiSideEffect {
+    data object NavigateToBack : GroupNickNameSideEffect
+
+    data object NavigateToNext : GroupNickNameSideEffect
 }
 
 @HiltViewModel
@@ -24,5 +33,18 @@ constructor(
     initialState = GroupNickNameUiState(),
 ) {
     override fun processIntent(intent: GroupNickNameIntent) {
+        when (intent) {
+            GroupNickNameIntent.ClickBackButton -> postSideEffect(GroupNickNameSideEffect.NavigateToBack)
+
+            GroupNickNameIntent.ClickNextButton -> {
+                postSideEffect(GroupNickNameSideEffect.NavigateToNext)
+            }
+
+            is GroupNickNameIntent.InputWord -> {
+                updateState {
+                    copy(nickName = intent.nickName)
+                }
+            }
+        }
     }
 }
