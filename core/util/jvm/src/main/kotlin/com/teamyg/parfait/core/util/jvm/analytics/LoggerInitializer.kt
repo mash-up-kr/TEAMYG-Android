3f5d34d681bccefb4aa60348d2307ae98b2372c7
citytexi <heekyne66@gmail.com>
2026-06-25 15:12:19 +0900

    feat: logger wrapping 구현

---
+++ core/util/jvm/src/main/kotlin/com/teamyg/parfait/core/util/jvm/analytics/LoggerInitializer.kt
@@ -0,0 +1,14 @@
+package com.teamyg.parfait.core.util.jvm.analytics
+
+import co.touchlab.kermit.platformLogWriter
+
+object LoggerInitializer {
+    fun setupDebug() {
+        co.touchlab.kermit.Logger
+            .setLogWriters(platformLogWriter())
+    }
+
+    fun setupRelease() {
+        // TODO Crashlytics Writer
+    }
+}
