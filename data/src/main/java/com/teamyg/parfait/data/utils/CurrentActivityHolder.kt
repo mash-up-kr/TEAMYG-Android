1b7e793650ff88c13fad0f216417fce5f11edecd
citytexi <heekyne66@gmail.com>
2026-06-27 12:56:23 +0900

    refactor: lint 수정

--- data/src/main/java/com/teamyg/parfait/data/utils/CurrentActivityHolder.kt
+++ data/src/main/java/com/teamyg/parfait/data/utils/CurrentActivityHolder.kt
@@ -28,13 +28,19 @@ constructor() : Application.ActivityLifecycleCallbacks {
         }
     }
 
-    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) = Unit
+    override fun onActivityCreated(
+        activity: Activity,
+        savedInstanceState: Bundle?,
+    ) = Unit
 
     override fun onActivityStarted(activity: Activity) = Unit
 
     override fun onActivityStopped(activity: Activity) = Unit
 
-    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) = Unit
+    override fun onActivitySaveInstanceState(
+        activity: Activity,
+        outState: Bundle,
+    ) = Unit
 
     override fun onActivityDestroyed(activity: Activity) = Unit
 }
