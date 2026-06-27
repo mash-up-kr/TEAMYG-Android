93eb5036c13874b56558aad09df08d16106756fb
citytexi <heekyne66@gmail.com>
2026-06-27 13:01:12 +0900

    feat: registerActivityLifecycleCallbacks 등록

--- app/src/main/java/com/teamyg/parfait/BaseApplication.kt
+++ app/src/main/java/com/teamyg/parfait/BaseApplication.kt
@@ -2,12 +2,18 @@ package com.teamyg.parfait
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import com.teamyg.parfait.data.utils.CurrentActivityHolder
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class BaseApplication : Application() {
+    @Inject
+    lateinit var currentActivityHolder: CurrentActivityHolder
+
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
+        registerActivityLifecycleCallbacks(currentActivityHolder)
     }
 }
