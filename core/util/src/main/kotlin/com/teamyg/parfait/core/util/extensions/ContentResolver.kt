275f52706d44e5ccf80c32685dd4612ddec62c4d
moon890309 <abcdefg8989@naver.com>
2026-06-28 12:56:56 +0900

    style: ktlint수정

--- core/util/src/main/kotlin/com/teamyg/parfait/core/util/extensions/ContentResolver.kt
+++ core/util/src/main/kotlin/com/teamyg/parfait/core/util/extensions/ContentResolver.kt
@@ -7,16 +7,14 @@ import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 
-fun ContentResolver.decodeUriToBitmap(uri: Uri): Bitmap {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-        val source = ImageDecoder.createSource(this, uri)
-        ImageDecoder.decodeBitmap(source) { decoder, info, _ ->
-            decoder.allocator = ImageDecoder.ALLOCATOR_SOFTWARE
-            decoder.isMutableRequired = true
-            decoder.setTargetSize(info.size.width, info.size.height)
-        }
-    } else {
-        @Suppress("DEPRECATION")
-        MediaStore.Images.Media.getBitmap(this, uri)
+fun ContentResolver.decodeUriToBitmap(uri: Uri): Bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+    val source = ImageDecoder.createSource(this, uri)
+    ImageDecoder.decodeBitmap(source) { decoder, info, _ ->
+        decoder.allocator = ImageDecoder.ALLOCATOR_SOFTWARE
+        decoder.isMutableRequired = true
+        decoder.setTargetSize(info.size.width, info.size.height)
     }
+} else {
+    @Suppress("DEPRECATION")
+    MediaStore.Images.Media.getBitmap(this, uri)
 }
