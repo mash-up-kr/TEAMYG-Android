f359a5ad1ebe7ad60cb7354ba6c3e972ff1c6d9f
citytexi <heekyne66@gmail.com>
2026-06-29 02:23:50 +0900

    feat: bitmap 랩핑용 모델 추가

---
+++ core/util/android/src/main/kotlin/com/teamyg/parfait/core/util/android/model/AndroidBitmap.kt
@@ -0,0 +1,12 @@
+package com.teamyg.parfait.core.util.android.model
+
+import android.graphics.Bitmap
+import com.teamyg.parfait.core.util.jvm.model.BitmapWrapper
+
+@JvmInline
+value class AndroidBitmap
+internal constructor(
+    private val delegate: Bitmap,
+) : BitmapWrapper {
+    // TODO delegate 사용하도록 수정
+}
