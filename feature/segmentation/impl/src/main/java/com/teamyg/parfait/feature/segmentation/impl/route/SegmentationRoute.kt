099f8e7b9b4410b01835900c0b98beb17b8a2dea
moon890309 <abcdefg8989@naver.com>
2026-07-03 06:24:43 +0900

    fix: SubjectSegmentation.getClient 에러 처리 구분

--- feature/segmentation/impl/src/main/java/com/teamyg/parfait/feature/segmentation/impl/route/SegmentationRoute.kt
+++ feature/segmentation/impl/src/main/java/com/teamyg/parfait/feature/segmentation/impl/route/SegmentationRoute.kt
@@ -1,14 +1,18 @@
 package com.teamyg.parfait.feature.segmentation.impl.route
 
+import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.teamyg.parfait.core.navigation.Navigator
 import com.teamyg.parfait.feature.canvas.api.NavKeyCanvasMove
 import com.teamyg.parfait.feature.segmentation.api.NavKeySegmentation
 import com.teamyg.parfait.feature.segmentation.impl.screen.SegmentationScreen
+import com.teamyg.parfait.feature.segmentation.impl.viewmodel.SegmentationEffect
 import com.teamyg.parfait.feature.segmentation.impl.viewmodel.SegmentationViewModel
 
 @Composable
@@ -21,6 +25,18 @@ internal fun SegmentationRoute(
         creationCallback = { factory -> factory.create(key.sourceImageUri) },
     )
     val state by viewModel.state.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+
+    LaunchedEffect(viewModel) {
+        viewModel.effect.collect { effect ->
+            when (effect){
+                is SegmentationEffect.SegmentationFailed -> {
+                    Toast.makeText(context, "객체 감지 실패", Toast.LENGTH_SHORT).show()
+                    navigator.onBack()
+                }
+            }
+        }
+    }
 
     SegmentationScreen(
         state = state,
