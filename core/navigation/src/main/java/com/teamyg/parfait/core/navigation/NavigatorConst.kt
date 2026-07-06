d12643edd4cf9614c51e486297f1a4151e031fdc
moon890309 <abcdefg8989@naver.com>
2026-07-07 06:36:07 +0900

    style: ktlint수정

--- core/navigation/src/main/java/com/teamyg/parfait/core/navigation/NavigatorConst.kt
+++ core/navigation/src/main/java/com/teamyg/parfait/core/navigation/NavigatorConst.kt
@@ -1,8 +1,7 @@
 package com.teamyg.parfait.core.navigation
 
-import com.teamyg.parfait.feature.groups.canvas.api.NavKeyCanvasImageAdd
 import com.teamyg.parfait.feature.intro.api.NavKeySplash
 
 object NavigatorConst {
-    val INITIAL_NAVIGATION_KEY = NavKeyCanvasImageAdd
+    val INITIAL_NAVIGATION_KEY = NavKeySplash
 }
