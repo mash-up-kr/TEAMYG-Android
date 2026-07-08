dd5c44c0242be718560eabf4ba5d54ae467012ac
moon890309 <abcdefg8989@naver.com>
2026-07-09 05:56:53 +0900

    feat: 알림 생성 및 권한 확인, 현재 등록된 토큰 가져오기

--- app/src/main/java/com/teamyg/parfait/MainActivity.kt
+++ app/src/main/java/com/teamyg/parfait/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.teamyg.parfait
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
-import com.teamyg.parfait.core.designsystem.theme.YGCustomTheme
+import com.google.firebase.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.messaging
+import com.teamyg.parfait.core.designsystem.theme.YGMaterialTheme
 import com.teamyg.parfait.core.navigation.Navigator
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
+private const val TAG = "MainActivity_Token"
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @Inject
@@ -21,11 +33,29 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var navigator: Navigator
 
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // 알람승인
+        } else {
+            // 알람거절
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d(TAG, "Token: ${task.result}")
+            } else{
+                Log.d(TAG, "Token Failed: ${task.exception}")
+            }
+        }
+        askNotificationPermission()
+        createNotification()
         enableEdgeToEdge()
         setContent {
-            YGCustomTheme {
+            YGMaterialTheme {
                 MainRoute(
                     navigator = navigator,
                     entryBuilders = entryBuilders,
@@ -33,5 +63,24 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+    }
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+    private fun createNotification(){
+        val channel = NotificationChannel(
+            "fcm_default_channel",
+            "파르페알림",
+            NotificationManager.IMPORTANCE_DEFAULT,
+        )
+        val manager = getSystemService(NotificationManager::class.java)
+        manager.createNotificationChannel(channel)
     }
 }
