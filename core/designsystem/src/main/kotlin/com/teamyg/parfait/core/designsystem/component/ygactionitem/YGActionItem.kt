b89307d3f54112bb267ea45cfc34ad3b03375745
JeonK1 <kan5016@naver.com>
2026-07-09 23:25:41 +0900

    YGActionITem 개발

---
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygactionitem/YGActionItem.kt
@@ -0,0 +1,48 @@
+package com.teamyg.parfait.core.designsystem.component.ygactionitem
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teamyg.parfait.core.designsystem.theme.YGTheme
+import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
+
+@Composable
+fun YGActionItem(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    interactionSource: MutableInteractionSource = MutableInteractionSource(),
+) {
+    val isPressed: Boolean by interactionSource.collectIsPressedAsState()
+    Box(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(vertical = 12.dp, horizontal = 16.dp),
+    ) {
+        Text(
+            text = text,
+            style = YGTheme.typography.body.b02R,
+            color = if (isPressed) YGAtomicColors.Gray.Gray700 else YGAtomicColors.Gray.Gray500,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun YGActionItemPreview() {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        YGActionItem(
+            text = "그룹 나가기",
+            onClick = {},
+        )
+    }
+}
