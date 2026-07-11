13eb5c95da19f593bb3a83e459e0ce73d1e1c4a9
JeonK1 <kan5016@naver.com>
2026-07-11 11:33:25 +0900

    Chore : ktlint format

--- core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/yginputnumber/YGInputNumber.kt
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/yginputnumber/YGInputNumber.kt
@@ -3,15 +3,12 @@ package com.teamyg.parfait.core.designsystem.component.yginputnumber
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
