f8b6355a854746f35e722f05dc33794d1a529df9
moon890309 <abcdefg8989@naver.com>
2026-07-12 12:39:35 +0900

    style: ktlint적용

--- domain/src/main/java/com/teamyg/parfait/domain/exception/SegmentationException.kt
+++ domain/src/main/java/com/teamyg/parfait/domain/exception/SegmentationException.kt
@@ -6,5 +6,6 @@ SegmentationException(
     cause: Throwable? = null,
 ) : Exception(message, cause) {
     class ImageNotFound(cause: Throwable?) : SegmentationException("bitmap 초기화 실패", cause)
+
     class ClientInit(cause: Throwable?) : SegmentationException("client 초기화 실패", cause)
 }
