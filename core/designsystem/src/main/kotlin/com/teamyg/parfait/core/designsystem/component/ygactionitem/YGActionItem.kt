ed90bf7ea9a9d074faed4cfbc4fecf5356593629
JeonK1 <kan5016@naver.com>
2026-07-12 18:05:12 +0900

    Chore : ktlint Format

--- core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygactionitem/YGActionItem.kt
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygactionitem/YGActionItem.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
