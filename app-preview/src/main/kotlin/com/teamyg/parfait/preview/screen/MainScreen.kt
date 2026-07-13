4cc56a9a3f2fc4bf4e2d733ab788af2b818844b4
citytexi <heekyne66@gmail.com>
2026-07-13 12:54:30 +0900

    refactor: rebase 충돌 내역 수정

--- app-preview/src/main/kotlin/com/teamyg/parfait/preview/screen/MainScreen.kt
+++ app-preview/src/main/kotlin/com/teamyg/parfait/preview/screen/MainScreen.kt
@@ -8,8 +8,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import com.teamyg.parfait.core.ui.preview.PreviewBox
-import com.teamyg.parfait.core.ui.preview.YGPreview
+import com.teamyg.parfait.core.designsystem.utils.preview.PreviewBox
+import com.teamyg.parfait.core.designsystem.utils.preview.YGPreview
 
 @Composable
 internal fun MainScreen(modifier: Modifier = Modifier) {
