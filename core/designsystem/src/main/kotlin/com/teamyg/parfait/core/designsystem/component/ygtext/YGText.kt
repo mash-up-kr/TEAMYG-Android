3ebb9f145d2f119c32a89f51e0ae8c791e4a5cc5
moon890309 <abcdefg8989@naver.com>
2026-07-15 06:52:42 +0900

    feat: label, date 공통 컴포넌트 구현

---
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygtext/YGText.kt
@@ -0,0 +1,51 @@
+package com.teamyg.parfait.core.designsystem.component.ygtext
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.teamyg.parfait.core.designsystem.theme.YGCustomTheme
+import com.teamyg.parfait.core.designsystem.theme.YGTheme
+import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
+
+@Composable
+fun YGLabel(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = text,
+        style = YGTheme.typography.body.b02R,
+        color = YGAtomicColors.Gray.Gray400,
+        modifier = modifier,
+    )
+}
+
+@Composable
+fun YGDate(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = text,
+        style = YGTheme.typography.body.b02R,
+        color = YGAtomicColors.Gray.Gray600,
+        modifier = modifier,
+    )
+}
+
+@Preview
+@Composable
+private fun YGLabelPreview() {
+    YGCustomTheme {
+        YGLabel(text = "레이블")
+    }
+}
+
+@Preview
+@Composable
+private fun YGDatePreview() {
+    YGCustomTheme {
+        YGDate(text = "7월 14일의 파르페")
+    }
+}
