05bcc65b080280e9069adaf56ab75ecd991bea23
JeonK1 <kan5016@naver.com>
2026-07-18 02:32:27 +0900

    Feature : DesignSystem 적용

--- feature/groups/enter/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/enter/impl/nickname/GroupNickNameScreen.kt
+++ feature/groups/enter/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/enter/impl/nickname/GroupNickNameScreen.kt
@@ -1,42 +1,29 @@
 package com.teamyg.parfait.feature.groups.enter.impl.nickname
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.teamyg.parfait.core.designsystem.component.textfield.YGTextFormField
+import com.teamyg.parfait.core.designsystem.component.textfield.YGTextFormFieldDefaults
+import com.teamyg.parfait.core.designsystem.component.ygbutton.YGButton
+import com.teamyg.parfait.core.designsystem.component.ygbutton.YGButtonType
+import com.teamyg.parfait.core.designsystem.component.ygtopbar.YGTopBarDetail
+import com.teamyg.parfait.core.designsystem.theme.YGTheme
 import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
-import com.teamyg.parfait.core.designsystem.theme.typography.YGTypography
-import com.teamyg.parfait.core.ui.preview.PreviewBox
-import com.teamyg.parfait.core.ui.preview.YGPreview
-import com.teamyg.parfait.core.designsystem.R as DesignSystemR
+import com.teamyg.parfait.core.designsystem.utils.preview.PreviewBox
+import com.teamyg.parfait.core.designsystem.utils.preview.YGPreview
+
+private const val NICKNAME_MAX_LENGTH = 15
 
 @Composable
 internal fun GroupNickNameScreen(
@@ -47,27 +34,17 @@ internal fun GroupNickNameScreen(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
-        // Todo : DesignSystem TopBar component 적용하기
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 12.dp, top = 16.dp, end = 16.dp, bottom = 16.dp),
-        ) {
-            Image(
-                painter = painterResource(DesignSystemR.drawable.ic_caret_left),
-                contentDescription = "뒤로가기",
-                modifier = Modifier
-                    .size(28.dp)
-                    .clickable { onClickBackButton() },
-            )
-            Spacer(modifier = Modifier.width(4.dp))
-            Text(
-                text = "그룹 참여하기",
-            )
-        }
+        YGTopBarDetail(
+            title = "그룹 참여하기",
+            onIconClick = onClickBackButton,
+            modifier = Modifier.fillMaxWidth(),
+        )
 
         LazyColumn(
-            contentPadding = PaddingValues(horizontal = 20.dp, vertical = 40.dp),
+            contentPadding = PaddingValues(
+                horizontal = YGTheme.layout.padding.padding7,
+                vertical = YGTheme.layout.padding.padding10,
+            ),
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f),
@@ -76,55 +53,40 @@ internal fun GroupNickNameScreen(
                 Text(
                     text = "그룹이름에서 사용할\n닉네임을 입력해 주세요",
                     color = YGAtomicColors.Gray.Gray900,
-                    style = YGTypography.Title.T02_B,
+                    style = YGTheme.typography.title.t02B,
                 )
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(YGTheme.layout.gap.gap3))
                 Text(
                     text = "그룹이름에서만 공유되는 닉네임이에요",
                     color = YGAtomicColors.Gray.Gray500,
-                    style = YGTypography.Body.B02_R,
+                    style = YGTheme.typography.body.b02R,
                 )
             }
             item {
-                Spacer(modifier = Modifier.height(49.dp))
-                // Todo : DesignSystem Input component 적용하기
-                BasicTextField(
+                Spacer(modifier = Modifier.height(YGTheme.layout.gap.gap10))
+
+                YGTextFormField(
                     value = uiState.nickName,
-                    textStyle = TextStyle(
-                        color = Color(0xFF000000),
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Normal,
-                    ),
                     onValueChange = { value -> onValueChanged(value) },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(8.dp))
-                        .border(width = 1.dp, color = Color(0xFFDFDFDF), shape = RoundedCornerShape(8.dp))
-                        .padding(start = 16.dp, end = 16.dp, top = 12.dp, bottom = 11.dp),
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = "닉네임을 입력해주세요", // Todo : placeholder 물어보기
+                    isError = false, // Todo : 작업 필요
+                    maxLength = NICKNAME_MAX_LENGTH,
+                    errorDescription = "", // Todo : 작업 필요
+                    colors = YGTextFormFieldDefaults.colors(),
                 )
             }
         }
-        Box(
+
+        YGButton(
+            text = "확인",
+            buttonType = YGButtonType.Large,
+            isEnabled = uiState.nickName.isNotEmpty(),
+            onClick = onClickNextButton,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(20.dp),
-        ) {
-            // Todo : DesignSystem Button component 적용하기
-            Text(
-                text = "확인",
-                color = Color(0xFFFFFFFF),
-                fontSize = 16.sp,
-                fontWeight = FontWeight.SemiBold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = Color(0xFF000000), shape = CircleShape)
-                    .border(width = 1.dp, color = Color(0xFFDFDFDF), shape = CircleShape)
-                    .clip(shape = CircleShape)
-                    .clickable(enabled = true) { onClickNextButton() }
-                    .padding(horizontal = 16.dp, vertical = 12.dp),
-            )
-        }
+                .padding(all = YGTheme.layout.padding.padding7),
+        )
     }
 }
 
