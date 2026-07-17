7fe21a63849ea2117f9d7a8099d0193de7ac0589
moon890309 <abcdefg8989@naver.com>
2026-07-17 21:17:30 +0900

    refactor: YGText를 YGDate, YGLabel로 분리

---
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygtext/YGDate.kt
@@ -0,0 +1,33 @@
+package com.teamyg.parfait.core.designsystem.component.ygtext
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teamyg.parfait.core.designsystem.theme.YGCustomTheme
+import com.teamyg.parfait.core.designsystem.theme.YGTheme
+import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
+
+
+@Composable
+fun YGDate(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = text,
+        style = YGTheme.typography.body.b02R,
+        color = YGAtomicColors.Gray.Gray600,
+        modifier = modifier.padding(horizontal = 12.dp, vertical = 8.dp),
+    )
+}
+
+@Preview
+@Composable
+private fun YGDatePreview() {
+    YGCustomTheme {
+        YGDate(text = "7월 14일의 파르페")
+    }
+}
