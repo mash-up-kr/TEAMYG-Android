bcaa8ba205a10f61c6b09adf61be78070bf9c374
moon890309 <abcdefg8989@naver.com>
2026-07-17 21:18:25 +0900

    fix: YGColorChip을 type과 size로 나누어 구분

--- core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygcolorchip/YGColorChip.kt
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygcolorchip/YGColorChip.kt
@@ -4,13 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -18,51 +22,53 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.teamyg.parfait.core.designsystem.component.ygcolorchip.YGColorChipType
 import com.teamyg.parfait.core.designsystem.theme.YGCustomTheme
 import com.teamyg.parfait.core.designsystem.theme.YGTheme
 
-sealed interface YGChipStyle {
-    val circleSize: Dp
+sealed interface YGColorChipStyle {
+    val colorChipSize: Dp
+    val colorChipWidth: Dp
     val textStyle: TextStyle
         @Composable get
 
-    data object Style28 : YGChipStyle {
-        override val circleSize = 28.dp
+    data object Style28 : YGColorChipStyle {
+        override val colorChipSize = 28.dp
+        override val colorChipWidth = 0.75.dp
         override val textStyle: TextStyle
             @Composable get() = YGTheme.typography.caption.c01R
     }
 
-    data object Style40 : YGChipStyle {
-        override val circleSize = 40.dp
+    data object Style40 : YGColorChipStyle {
+        override val colorChipSize = 40.dp
+        override val colorChipWidth = 1.dp
         override val textStyle: TextStyle
             @Composable get() = YGTheme.typography.body.b01R
     }
 }
 
 @Composable
-fun YGChip(
-    fillColor: Color,
-    strokeColor: Color,
-    textColor: Color,
+fun YGColorChip(
+    colorChipType: YGColorChipType,
     text: String,
-    chip: YGChipStyle,
+    chip: YGColorChipStyle,
     modifier: Modifier = Modifier,
 ) {
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
-            .size(chip.circleSize)
+            .size(chip.colorChipSize)
             .clip(CircleShape)
-            .background(fillColor)
+            .background(colorChipType.fillColor)
             .border(
-                width = 0.75.dp,
-                color = strokeColor,
+                width = chip.colorChipWidth,
+                color = colorChipType.strokeColor,
                 shape = CircleShape,
             ),
     ) {
         Text(
             text = text,
-            color = textColor,
+            color = colorChipType.textColor,
             style = chip.textStyle,
         )
     }
@@ -71,25 +77,28 @@ fun YGChip(
 @Preview
 @Composable
 private fun YGChipPreview(
-    @PreviewParameter(YGChipPreviewParameterProvider::class)
+    @PreviewParameter(YGColorChipPreviewParameterProvider::class)
     data: YGChipPreviewData,
 ) {
     YGCustomTheme {
-        Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
-            YGChip(
-                fillColor = data.fillColor,
-                strokeColor = data.strokeColor,
-                textColor = data.textColor,
-                text = "문",
-                chip = YGChipStyle.Style28,
-            )
-            YGChip(
-                fillColor = data.fillColor,
-                strokeColor = data.strokeColor,
-                textColor = data.textColor,
-                text = "문",
-                chip = YGChipStyle.Style40,
-            )
+        Column(
+        ) {
+            Text(data.name)
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                YGColorChip(
+                    colorChipType = data.colorChipType,
+                    text = "문",
+                    chip = YGColorChipStyle.Style28,
+                )
+                YGColorChip(
+                    colorChipType = data.colorChipType,
+                    text = "문",
+                    chip = YGColorChipStyle.Style40,
+                )
+            }
         }
+
     }
 }
---
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygcolorchip/YGColorChipPreviewData.kt
@@ -0,0 +1,72 @@
+package com.teamyg.parfait.core.designsystem.component.ygchip
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import com.teamyg.parfait.core.designsystem.component.ygcolorchip.YGColorChipType
+import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
+
+data class YGChipPreviewData(
+    val name : String,
+    val colorChipType: YGColorChipType,
+)
+
+class YGColorChipPreviewParameterProvider : PreviewParameterProvider<YGChipPreviewData> {
+    override val values = sequenceOf(
+        YGChipPreviewData(
+            name = "nametagChip1",
+            colorChipType = YGColorChipType.nametagChip1
+        ),
+        YGChipPreviewData(
+            name = "nametagChip2",
+            colorChipType = YGColorChipType.nametagChip2
+        ),
+        YGChipPreviewData(
+            name = "nametagChip3",
+            colorChipType = YGColorChipType.nametagChip3
+        ),
+        YGChipPreviewData(
+            name = "nametagChip4",
+            colorChipType = YGColorChipType.nametagChip4
+        ),
+        YGChipPreviewData(
+            name = "nametagChip5",
+            colorChipType = YGColorChipType.nametagChip5
+        ),
+        YGChipPreviewData(
+            name = "nametagChip6",
+            colorChipType = YGColorChipType.nametagChip6
+        ),
+        YGChipPreviewData(
+            name = "nametagChip7",
+            colorChipType = YGColorChipType.nametagChip7
+        ),
+        YGChipPreviewData(
+            name = "nametagChip8",
+            colorChipType = YGColorChipType.nametagChip8
+        ),
+        YGChipPreviewData(
+            name = "nametagChip9",
+            colorChipType = YGColorChipType.nametagChip9
+        ),
+        YGChipPreviewData(
+            name = "nametagChip10",
+            colorChipType = YGColorChipType.nametagChip10
+        ),
+        YGChipPreviewData(
+            name = "nametagChip11",
+            colorChipType = YGColorChipType.nametagChip11
+        ),
+        YGChipPreviewData(
+            name = "nametagChip12",
+            colorChipType = YGColorChipType.nametagChip12
+        ),
+        YGChipPreviewData(
+            name = "nametagChip13",
+            colorChipType = YGColorChipType.nametagChip13
+        ),
+        YGChipPreviewData(
+            name = "nametagChipPlus",
+            colorChipType = YGColorChipType.nametagChipPlus
+        ),
+    )
+}
