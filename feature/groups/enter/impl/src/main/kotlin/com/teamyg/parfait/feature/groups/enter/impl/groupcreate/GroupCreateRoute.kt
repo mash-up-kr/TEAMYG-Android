411f28af76e979925d3aeb6ce0247ff9084d5283
JeonK1 <kan5016@naver.com>
2026-07-20 21:41:55 +0900

    Feature : UI 구현

--- feature/groups/enter/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/enter/impl/groupcreate/GroupCreateRoute.kt
+++ feature/groups/enter/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/enter/impl/groupcreate/GroupCreateRoute.kt
@@ -1,9 +1,15 @@
 package com.teamyg.parfait.feature.groups.enter.impl.groupcreate
 
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.teamyg.parfait.core.navigation.Navigator
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -13,6 +19,31 @@ fun GroupCreateRoute(
     modifier: Modifier = Modifier,
     viewModel: GroupCreateViewModel = hiltViewModel(),
 ) {
+    val uiState by viewModel.state.collectAsStateWithLifecycle()
+    val imeVisible = WindowInsets.isImeVisible
+
+    LaunchedEffect(Unit) {
+        viewModel.effect.collect { effect ->
+            when (effect) {
+                GroupCreateSideEffect.NavigateToBack -> {
+                    navigator.onBack()
+                }
+
+                GroupCreateSideEffect.NavigateToNext -> { /* navigate to next */ }
+            }
+        }
+    }
+
+    LaunchedEffect(imeVisible) {
+        if (imeVisible.not()) {
+            viewModel.processIntent(GroupCreateIntent.HideKeyboard)
+        }
+    }
+
     GroupCreateScreen(
+        uiState = uiState,
+        onClickNextButton = { viewModel.processIntent(GroupCreateIntent.ClickNextButton) },
+        onClickBackButton = { viewModel.processIntent(GroupCreateIntent.ClickBackButton) },
+        modifier = modifier.imePadding(),
     )
 }
