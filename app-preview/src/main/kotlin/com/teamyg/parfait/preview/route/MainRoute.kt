4f0424ecfaa2b8a4db3b5a1707ce928d48a5e53d
moon890309 <abcdefg8989@naver.com>
2026-07-21 06:48:20 +0900

    chore: 테스트 코드

--- app-preview/src/main/kotlin/com/teamyg/parfait/preview/route/MainRoute.kt
+++ app-preview/src/main/kotlin/com/teamyg/parfait/preview/route/MainRoute.kt
@@ -2,6 +2,7 @@ package com.teamyg.parfait.preview.route
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.teamyg.parfait.core.designsystem.component.ygtoast.rememberYGToastPolicy
 import com.teamyg.parfait.core.navigation.Navigator
 import com.teamyg.parfait.preview.screen.MainScreen
 
@@ -10,7 +11,9 @@ internal fun MainRoute(
     navigator: Navigator,
     modifier: Modifier = Modifier,
 ) {
+    val toastPolicy = rememberYGToastPolicy()
     MainScreen(
+        toast = toastPolicy,
         modifier = modifier,
     )
 }
--- app-preview/src/main/kotlin/com/teamyg/parfait/preview/screen/MainScreen.kt
+++ app-preview/src/main/kotlin/com/teamyg/parfait/preview/screen/MainScreen.kt
@@ -3,23 +3,37 @@ package com.teamyg.parfait.preview.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import com.teamyg.parfait.core.designsystem.component.ygtoast.YGToastHost
+import com.teamyg.parfait.core.designsystem.component.ygtoast.YGToastPolicy
+import com.teamyg.parfait.core.designsystem.component.ygtoast.YGToastType
+import com.teamyg.parfait.core.designsystem.component.ygtoast.rememberYGToastPolicy
 import com.teamyg.parfait.core.designsystem.utils.preview.PreviewBox
 import com.teamyg.parfait.core.designsystem.utils.preview.YGPreview
 
 @Composable
-internal fun MainScreen(modifier: Modifier = Modifier) {
+internal fun MainScreen(
+    toast: YGToastPolicy,
+    modifier: Modifier = Modifier,
+) {
     Box(
-        modifier = modifier
-            .background(color = Color.White),
+        modifier = modifier.background(color = Color.White),
         contentAlignment = Alignment.Center,
     ) {
-        Text(
-            text = "Preview Application",
+        Button(onClick = {
+            toast.show(YGToastType.Edit("토스트 테스트"))
+            toast.show(YGToastType.InviteCode("토스트 테스트22"))
+        }) {
+            Text("토스트 띄우기")
+        }
+        YGToastHost(
+            policy = toast,
+            modifier = Modifier.align(Alignment.TopCenter),
         )
     }
 }
@@ -28,6 +42,7 @@ internal fun MainScreen(modifier: Modifier = Modifier) {
 @Composable
 private fun PreviewMainScreen() = PreviewBox {
     MainScreen(
+        toast = rememberYGToastPolicy(),
         modifier = Modifier.fillMaxSize(),
     )
 }
