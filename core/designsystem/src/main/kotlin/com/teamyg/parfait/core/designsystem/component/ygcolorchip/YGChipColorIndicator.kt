b556fcf7077f520280e99d7be2c72aea3a7b034b
moon890309 <abcdefg8989@naver.com>
2026-07-22 07:08:38 +0900

    feat›: YGChipColorIndicator구현

---
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygcolorchip/YGChipColorIndicator.kt
@@ -0,0 +1,30 @@
+package com.teamyg.parfait.core.designsystem.component.ygcolorchip
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
+
+@Composable
+fun YGChipColorIndicator(
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier
+            .size(4.dp)
+            .clip(CircleShape)
+            .background(YGAtomicColors.Cherry.Cherry)
+    )
+}
+
+@Preview
+@Composable
+private fun YGChipColorIndicatorPreview(){
+    YGChipColorIndicator()
+}
