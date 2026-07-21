1cbebaacaf1a8ea91b2351c645e2b00beb13f930
citytexi <heekyne66@gmail.com>
2026-07-21 09:30:37 +0900

    refactor: Surface 기본 컨테이너 색상 변경

--- core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/screen/YGScreen.kt
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/screen/YGScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
 
 @Composable
 fun YGScreen(
@@ -12,6 +13,7 @@ fun YGScreen(
 ) {
     Surface(
         modifier = modifier,
+        color = YGAtomicColors.Gray.Transparent,
     ) {
         val scope = remember { YGScreenScope() }
         scope.content()
