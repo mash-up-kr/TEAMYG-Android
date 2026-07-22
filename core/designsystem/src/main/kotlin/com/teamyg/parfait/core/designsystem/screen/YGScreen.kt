31b26c359513b9126123965a83ac537f9e20c1a8
citytexi <heekyne66@gmail.com>
2026-07-23 08:45:36 +0900

    feat: YGScreen focus clear 도입

--- core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/screen/YGScreen.kt
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/screen/YGScreen.kt
@@ -4,15 +4,22 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
+import com.teamyg.parfait.core.util.android.clickable.clickableYGNoRipple
 
 @Composable
 fun YGScreen(
     modifier: Modifier = Modifier,
     content: @Composable YGScreenScope.() -> Unit,
 ) {
+    val focusManager = LocalFocusManager.current
+
     Surface(
-        modifier = modifier,
+        modifier = modifier
+            .clickableYGNoRipple {
+                focusManager.clearFocus()
+            },
         color = YGAtomicColors.Gray.Transparent,
     ) {
         val scope = remember { YGScreenScope() }
