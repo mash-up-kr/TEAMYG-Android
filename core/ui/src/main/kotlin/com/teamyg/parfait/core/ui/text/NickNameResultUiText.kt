37dac3c8558662f177eac6d14966ee73150eda23
citytexi <heekyne66@gmail.com>
2026-07-23 08:45:36 +0900

    feat: YGScreen focus clear 도입

--- core/ui/src/main/kotlin/com/teamyg/parfait/core/ui/text/NickNameResultUiText.kt
+++ core/ui/src/main/kotlin/com/teamyg/parfait/core/ui/text/NickNameResultUiText.kt
@@ -3,12 +3,12 @@ package com.teamyg.parfait.core.ui.text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.teamyg.parfait.core.ui.R
-import com.teamyg.parfait.domain.model.NickNameResult
+import com.teamyg.parfait.domain.model.NicknameResult
 
 @Composable
-fun NickNameResult.Error.toStringResource(): String = when (this) {
-    NickNameResult.Error.Empty -> stringResource(R.string.nickname_error_empty)
-    NickNameResult.Error.SpaceAtEdge -> stringResource(R.string.nickname_error_space_at_edge)
-    NickNameResult.Error.DuplicatedSpace -> stringResource(R.string.nickname_error_duplicated_space)
-    NickNameResult.Error.InvalidCharacter -> stringResource(R.string.nickname_error_invalid_character)
+fun NicknameResult.Error.toStringResource(): String = when (this) {
+    NicknameResult.Error.Empty -> stringResource(R.string.nickname_error_empty)
+    NicknameResult.Error.SpaceAtEdge -> stringResource(R.string.nickname_error_space_at_edge)
+    NicknameResult.Error.DuplicatedSpace -> stringResource(R.string.nickname_error_duplicated_space)
+    NicknameResult.Error.InvalidCharacter -> stringResource(R.string.nickname_error_invalid_character)
 }
