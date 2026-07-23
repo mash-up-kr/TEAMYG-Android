38674298e324431a3b75dd2777e9596bdb50649f
JeonK1 <kan5016@naver.com>
2026-07-23 23:37:33 +0900

    Refactor : NameValidation inline

--- domain/src/main/java/com/teamyg/parfait/domain/usecase/group/CheckNameValidUseCase.kt
+++ domain/src/main/java/com/teamyg/parfait/domain/usecase/group/CheckNameValidUseCase.kt
@@ -1,5 +1,6 @@
 package com.teamyg.parfait.domain.usecase.group
 
+import com.teamyg.parfait.core.util.jvm.extension.isKorean
 import com.teamyg.parfait.domain.model.NameValidResult
 import javax.inject.Inject
 
@@ -15,4 +16,30 @@ constructor() {
 
         return NameValidResult.Success
     }
+}
+
+private enum class NameValidation(
+    val isValid: (String) -> Boolean,
+    val errorType: NameValidResult.Error,
+) {
+    CheckSpaceStartOrEnd(
+        isValid = { nickName ->
+            nickName.startsWith(" ").not() && nickName.endsWith(" ").not()
+        },
+        errorType = NameValidResult.Error.SpaceAtEdge,
+    ),
+
+    CheckDuplicatedSpace(
+        isValid = { nickName ->
+            nickName.indexOf("  ") == -1
+        },
+        errorType = NameValidResult.Error.DuplicatedSpace,
+    ),
+
+    CheckValidCharacter(
+        isValid = { nickName ->
+            nickName.all { it.isWhitespace() || it.isDigit() || it.isLetter() || it.isKorean() }
+        },
+        errorType = NameValidResult.Error.InvalidCharacter,
+    ),
 }
