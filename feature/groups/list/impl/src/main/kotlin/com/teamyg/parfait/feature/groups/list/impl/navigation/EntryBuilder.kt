eae1ae414abb33686beebea929f207b27cc2fa45
JeonK1 <kan5016@naver.com>
2026-07-25 00:12:25 +0900

    Feature : 배경 추가

--- feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/navigation/EntryBuilder.kt
+++ feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/navigation/EntryBuilder.kt
@@ -1,18 +1,28 @@
 package com.teamyg.parfait.feature.groups.list.impl.navigation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.teamyg.parfait.core.designsystem.screen.YGScaffold
 import com.teamyg.parfait.core.navigation.Navigator
 import com.teamyg.parfait.feature.groups.list.api.NavKeyGroupList
+import com.teamyg.parfait.feature.groups.list.impl.R
 import com.teamyg.parfait.feature.groups.list.impl.route.GroupListRoute
 
 fun EntryProviderScope<NavKey>.featureGroupListEntryBuilder(navigator: Navigator) {
     entry<NavKeyGroupList> {
         YGScaffold { innerPadding ->
+            Image(
+                painter = painterResource(R.drawable.group_list_background),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize(),
+            )
             GroupListRoute(
                 navigator = navigator,
                 modifier = Modifier
