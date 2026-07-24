b3e11d66c4d4bd6171d5a3b39c8e58f59e52a900
JeonK1 <kan5016@naver.com>
2026-07-25 00:46:44 +0900

    Chore : 임시 파르페 구현

--- feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/GroupListScreen.kt
+++ feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/GroupListScreen.kt
@@ -1,16 +1,29 @@
 package com.teamyg.parfait.feature.groups.list.impl.route
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.teamyg.parfait.core.designsystem.R
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.teamyg.parfait.core.designsystem.component.ygchipbutton.YGChipButton
 import com.teamyg.parfait.core.designsystem.component.ygchipbutton.YGChipButtonColorsDefaults
 import com.teamyg.parfait.core.designsystem.component.ygtext.YGDate
 import com.teamyg.parfait.core.designsystem.component.ygtopbar.YGTopBarEmpty
+import com.teamyg.parfait.core.designsystem.theme.YGTheme
+import com.teamyg.parfait.feature.groups.list.impl.R
+import com.teamyg.parfait.core.designsystem.R as DesignSystemR
 
 @Composable
 fun GroupListScreen(
@@ -31,17 +44,86 @@ fun GroupListScreen(
                         text = "그룹 추가하기",
                         colors = YGChipButtonColorsDefaults.CherryBackgroundPressed,
                         onClick = {},
-                        startIconResource = R.drawable.ic_plus,
+                        startIconResource = DesignSystemR.drawable.ic_plus,
                     )
                 },
             )
 
-            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            LazyColumn(
+                contentPadding = PaddingValues(
+                    start = YGTheme.layout.padding.padding7,
+                    end = YGTheme.layout.padding.padding7,
+                    bottom = YGTheme.layout.padding.padding6,
+                ),
+                modifier = Modifier.fillMaxWidth(),
+            ) {
                 item {
                     YGDate(
                         date = uiState.dateString,
                         day = uiState.dayOfWeekString,
                     )
+                }
+                item {
+                    // Todo : 임시코드
+                    Spacer(modifier = Modifier.height(24.dp))
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.parfait_cherry),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillWidth,
+                            modifier = Modifier
+                                .width(83.dp)
+                                .zIndex(0f),
+                        )
+                        Image(
+                            painter = painterResource(R.drawable.parfait_cream_top),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillWidth,
+                            modifier = Modifier
+                                .width(210.dp)
+                                .offset(y = (-40).dp)
+                                .zIndex(0f),
+                        )
+                        Image(
+                            painter = painterResource(R.drawable.parfait_cream_default),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillWidth,
+                            modifier = Modifier
+                                .width(241.dp)
+                                .offset(y = (-40-44).dp)
+                                .zIndex(-1f),
+                        )
+                        Image(
+                            painter = painterResource(R.drawable.parfait_cream_default),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillWidth,
+                            modifier = Modifier
+                                .width(241.dp)
+                                .offset(y = (-40-44-66).dp)
+                                .zIndex(-2f),
+                        )
+                        Image(
+                            painter = painterResource(R.drawable.parfait_cream_default),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillWidth,
+                            modifier = Modifier
+                                .width(241.dp)
+                                .offset(y = (-40-44-66-66).dp)
+                                .zIndex(-3f),
+                        )
+                        Image(
+                            painter = painterResource(R.drawable.parfait_cup),
+                            contentDescription = null,
+                            contentScale = ContentScale.FillWidth,
+                            modifier = Modifier
+                                .width(324.dp)
+                                .offset(y = (-40-44-66-66-32).dp)
+                                .zIndex(0f),
+                        )
+                    }
                 }
             }
         }
