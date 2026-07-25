7c103dc47dc4673763d822e08e4eb471be924ea9
moon890309 <abcdefg8989@naver.com>
2026-07-25 13:21:48 +0900

    fix: 셔터 버튼 디자인에 맞춰 수정

--- feature/camera/impl/src/main/java/com/teamyg/parfait/feature/camera/impl/component/controls/ShutterButton.kt
+++ feature/camera/impl/src/main/java/com/teamyg/parfait/feature/camera/impl/component/controls/ShutterButton.kt
@@ -2,14 +2,20 @@ package com.teamyg.parfait.feature.camera.impl.component.controls
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
 import com.teamyg.parfait.core.designsystem.utils.preview.PreviewBox
 import com.teamyg.parfait.core.designsystem.utils.preview.YGPreview
 
@@ -18,18 +24,18 @@ internal fun ShutterButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    IconButton(
-        onClick = onClick,
+    Box(
         modifier = modifier
-            .size(72.dp)
+            .size(56.dp)
             .clip(CircleShape)
-            .background(Color.White)
-            .border(
-                width = 4.dp,
-                color = Color.Gray,
-                shape = CircleShape,
-            ),
-    ) {}
+            .border(width = 1.dp, color = YGAtomicColors.Transparency.Black5, shape = CircleShape)
+            .clickable{ onClick() },
+        contentAlignment = Alignment.Center,
+    ){
+        Box(
+            modifier = Modifier.size(48.dp).clip(CircleShape).background(color = YGAtomicColors.Gray.Gray900),
+        )
+    }
 }
 
 @YGPreview
