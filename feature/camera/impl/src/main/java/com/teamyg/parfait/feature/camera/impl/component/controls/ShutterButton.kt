f9fdafe17ffbcf041e92894e311a308033902d5f
moon890309 <abcdefg8989@naver.com>
2026-07-26 23:07:32 +0900

    fix: 셔터버튼 디자인에 맞춰 수정

--- feature/camera/impl/src/main/java/com/teamyg/parfait/feature/camera/impl/component/controls/ShutterButton.kt
+++ feature/camera/impl/src/main/java/com/teamyg/parfait/feature/camera/impl/component/controls/ShutterButton.kt
@@ -4,16 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
 import com.teamyg.parfait.core.designsystem.utils.preview.PreviewBox
@@ -29,9 +26,9 @@ internal fun ShutterButton(
             .size(56.dp)
             .clip(CircleShape)
             .border(width = 1.dp, color = YGAtomicColors.Transparency.Black5, shape = CircleShape)
-            .clickable{ onClick() },
+            .clickable { onClick() },
         contentAlignment = Alignment.Center,
-    ){
+    ) {
         Box(
             modifier = Modifier.size(48.dp).clip(CircleShape).background(color = YGAtomicColors.Gray.Gray900),
         )
