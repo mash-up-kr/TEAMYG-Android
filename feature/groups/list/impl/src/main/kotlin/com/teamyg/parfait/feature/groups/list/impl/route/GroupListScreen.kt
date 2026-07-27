03056c072b23b77b99b083a9bcd9e7f44e6bbbe5
JeonK1 <kan5016@naver.com>
2026-07-27 22:06:01 +0900

    GroupListParfaitLayout 구현

--- feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/GroupListScreen.kt
+++ feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/GroupListScreen.kt
@@ -3,14 +3,11 @@ package com.teamyg.parfait.feature.groups.list.impl.route
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TooltipAnchorPosition
 import androidx.compose.material3.TooltipBox
@@ -18,14 +15,12 @@ import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import com.teamyg.parfait.core.designsystem.component.ygchipbutton.YGChipButton
 import com.teamyg.parfait.core.designsystem.component.ygchipbutton.YGChipButtonColorsDefaults
 import com.teamyg.parfait.core.designsystem.component.ygtext.YGDate
@@ -108,66 +103,42 @@ internal fun GroupListScreen(
 }
 
 @Composable
-fun GroupListContent(modifier: Modifier = Modifier) {
-    // Todo : 임시코드
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxWidth(),
-    ) {
-        Image(
-            painter = painterResource(R.drawable.parfait_cherry),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .width(83.dp)
-                .zIndex(0f),
-        )
-        Image(
-            painter = painterResource(R.drawable.parfait_cream_top),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .width(210.dp)
-                .offset(y = (-40).dp)
-                .zIndex(0f),
-        )
-        Image(
-            painter = painterResource(R.drawable.parfait_cream_default),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .width(241.dp)
-                .offset(y = (-40 - 44).dp)
-                .zIndex(-1f),
-        )
-        Image(
-            painter = painterResource(R.drawable.parfait_cream_default),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .width(241.dp)
-                .offset(y = (-40 - 44 - 66).dp)
-                .zIndex(-2f),
-        )
-        Image(
-            painter = painterResource(R.drawable.parfait_cream_default),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .width(241.dp)
-                .offset(y = (-40 - 44 - 66 - 66).dp)
-                .zIndex(-3f),
-        )
-        Image(
-            painter = painterResource(R.drawable.parfait_cup),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier
-                .width(324.dp)
-                .offset(y = (-40 - 44 - 66 - 66 - 32).dp)
-                .zIndex(0f),
-        )
-    }
+internal fun GroupListContent(modifier: Modifier = Modifier) {
+    GroupListParfaitLayout(
+        cherrySection = {
+            Image(
+                painter = painterResource(R.drawable.parfait_cherry),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.width(83.dp),
+            )
+        },
+        topSection = {
+            Image(
+                painter = painterResource(R.drawable.parfait_cream_top),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.width(210.dp),
+            )
+        },
+        middleSection = {
+            Image(
+                painter = painterResource(R.drawable.parfait_cream_default),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.width(241.dp),
+            )
+        },
+        bottomSection = {
+            Image(
+                painter = painterResource(R.drawable.parfait_cup),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.width(324.dp),
+            )
+        },
+        modifier = modifier.fillMaxWidth(),
+    )
 }
 
 private class GroupListScreenPreviewParameterProvider :
