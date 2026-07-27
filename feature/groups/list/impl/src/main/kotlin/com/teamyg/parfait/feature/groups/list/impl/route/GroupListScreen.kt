0c866c122bafc1d88a8c88ff30bc1cfeba5c18f3
JeonK1 <kan5016@naver.com>
2026-07-27 22:45:50 +0900

    Chore : 파일 옮기기

--- feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/GroupListScreen.kt
+++ feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/GroupListScreen.kt
@@ -1,8 +1,6 @@
 package com.teamyg.parfait.feature.groups.list.impl.route
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -10,11 +8,9 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
 import androidx.compose.material3.TooltipAnchorPosition
 import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
@@ -25,8 +21,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
@@ -36,12 +30,10 @@ import com.teamyg.parfait.core.designsystem.component.ygchipbutton.YGChipButtonC
 import com.teamyg.parfait.core.designsystem.component.ygtext.YGDate
 import com.teamyg.parfait.core.designsystem.component.ygtopbar.YGTopBarEmpty
 import com.teamyg.parfait.core.designsystem.theme.YGTheme
-import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
 import com.teamyg.parfait.core.designsystem.utils.preview.PreviewBox
 import com.teamyg.parfait.core.designsystem.utils.preview.YGPreview
-import com.teamyg.parfait.core.util.android.extension.drawTooltipCornerTopRight
-import com.teamyg.parfait.core.util.android.extension.withStyle
 import com.teamyg.parfait.feature.groups.list.impl.R
+import com.teamyg.parfait.feature.groups.list.impl.route.component.GroupListTooltip
 import com.teamyg.parfait.core.designsystem.R as DesignSystemR
 
 @OptIn(ExperimentalMaterial3Api::class)
--- feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/component/GroupListTooltip.kt
+++ feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/component/GroupListTooltip.kt
@@ -1,4 +1,4 @@
-package com.teamyg.parfait.feature.groups.list.impl.route
+package com.teamyg.parfait.feature.groups.list.impl.route.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
