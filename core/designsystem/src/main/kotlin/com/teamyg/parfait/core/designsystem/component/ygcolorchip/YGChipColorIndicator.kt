83400fc356d6a2b0360dd9005f3274da328db067
moon890309 <abcdefg8989@naver.com>
2026-07-30 20:48:39 +0900

    feat: colorIndicator 색상 분기 추가

--- core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygcolorchip/YGChipColorIndicator.kt
+++ core/designsystem/src/main/kotlin/com/teamyg/parfait/core/designsystem/component/ygcolorchip/YGChipColorIndicator.kt
@@ -2,28 +2,35 @@ package com.teamyg.parfait.core.designsystem.component.ygcolorchip
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.teamyg.parfait.core.designsystem.theme.colors.YGAtomicColors
 import com.teamyg.parfait.core.designsystem.utils.preview.PreviewBox
 import com.teamyg.parfait.core.designsystem.utils.preview.YGPreview
 
 @Composable
-fun YGChipColorIndicator(modifier: Modifier = Modifier) {
+fun YGChipColorIndicator(modifier: Modifier = Modifier, isChecked: Boolean) {
+    val color = if(isChecked) YGAtomicColors.Cherry.Cherry else Color.Transparent
     Box(
         modifier = modifier
             .size(4.dp)
             .clip(CircleShape)
-            .background(YGAtomicColors.Cherry.Cherry),
+            .background(color),
     )
 }
 
 @YGPreview
 @Composable
 private fun YGChipColorIndicatorPreview() = PreviewBox {
-    YGChipColorIndicator()
+    Column() {
+        YGChipColorIndicator(isChecked = true)
+        YGChipColorIndicator(isChecked = false)
+    }
+
 }
