be635e1fe39b34bf9ec25282371f9dd88bcd42f1
moon890309 <abcdefg8989@naver.com>
2026-08-01 15:29:26 +0900

    chore: ktlintFormat

--- feature/camera/impl/src/main/java/com/teamyg/parfait/feature/camera/impl/component/CameraControlComponent.kt
+++ feature/camera/impl/src/main/java/com/teamyg/parfait/feature/camera/impl/component/CameraControlComponent.kt
@@ -4,11 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.teamyg.parfait.core.designsystem.R
 import com.teamyg.parfait.core.designsystem.component.ygcamerashutter.YGCameraShutter
 import com.teamyg.parfait.core.designsystem.component.ygcirclebutton.YGCircleButton
