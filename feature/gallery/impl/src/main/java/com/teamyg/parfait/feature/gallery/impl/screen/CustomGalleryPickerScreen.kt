c99e14a6a30a7a5dde48297427bb459c54e9f515
moon890309 <abcdefg8989@naver.com>
2026-08-03 07:03:51 +0900

    chore:빈화면 이미지 추가

--- feature/gallery/impl/src/main/java/com/teamyg/parfait/feature/gallery/impl/screen/CustomGalleryPickerScreen.kt
+++ feature/gallery/impl/src/main/java/com/teamyg/parfait/feature/gallery/impl/screen/CustomGalleryPickerScreen.kt
@@ -1,5 +1,6 @@
 package com.teamyg.parfait.feature.gallery.impl.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -124,12 +126,21 @@ private fun GalleryContent(
                 when {
                     isLoading -> CircularProgressIndicator(color = Color.White)
 
-                    isEmpty -> Text(
-                        text = stringResource(R.string.gallery_empty_today_description),
-                        color = YGAtomicColors.Gray.Gray300,
-                        style = YGTheme.typography.body.b02R,
-                        textAlign = TextAlign.Center,
-                    )
+                    isEmpty -> Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.spacedBy(YGTheme.layout.gap.gap1),
+                    ) {
+                        Image(
+                            painter = painterResource(DesignSystemR.drawable.image_gallery_empty),
+                            contentDescription = null,
+                        )
+                        Text(
+                            text = stringResource(R.string.gallery_empty_today_description),
+                            color = YGAtomicColors.Gray.Gray300,
+                            style = YGTheme.typography.body.b02R,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
 
                     else -> GalleryImageGridComponent(
                         groups = groups,
