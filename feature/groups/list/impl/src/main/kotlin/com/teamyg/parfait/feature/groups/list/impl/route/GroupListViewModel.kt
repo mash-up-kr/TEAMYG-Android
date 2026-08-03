7201d4b4ee1c086e0f56c1c89bb20774f2bdfd57
JeonK1 <kan5016@naver.com>
2026-08-03 22:42:24 +0900

    Reapply "Merge pull request #187 from mash-up-kr/feature/#169-group-list-item"
    This reverts commit 0dd53e016c9695889864ccb55ae68bfcfb1abcab.

--- feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/GroupListViewModel.kt
+++ feature/groups/list/impl/src/main/kotlin/com/teamyg/parfait/feature/groups/list/impl/route/GroupListViewModel.kt
@@ -1,5 +1,6 @@
 package com.teamyg.parfait.feature.groups.list.impl.route
 
+import com.teamyg.parfait.core.designsystem.component.yggrouptagchip.YGGrouptagChipType
 import com.teamyg.parfait.core.ui.BaseViewModel
 import com.teamyg.parfait.core.ui.UiIntent
 import com.teamyg.parfait.core.ui.UiSideEffect
@@ -12,8 +13,38 @@ import kotlinx.datetime.toLocalDateTime
 import javax.inject.Inject
 import kotlin.time.Clock
 
+data class MockToppingGroup(
+    val name: String,
+    val imageUrl: String,
+    val lastModify: String,
+) {
+    val chipType: YGGrouptagChipType = YGGrouptagChipType.TYPE_1_2
+}
+
 data class GroupListUiState(
-    val groupList: List<String> = emptyList(), // Todo : 임시로 String 으로 설정하였습니다
+    // Todo : 임시로 MockToppingGroup 으로 설정하였습니다
+    val groupList: List<MockToppingGroup> = listOf(
+        MockToppingGroup(
+            "매시업1",
+            "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201611/16/htm_2016111623638229254.png",
+            "3분전",
+        ),
+        MockToppingGroup(
+            "매시업매시업매시업",
+            "https://i.pinimg.com/236x/d8/a6/cb/d8a6cbb02bc2c5c27ae238db2e89425d.jpg",
+            "10분전",
+        ),
+        MockToppingGroup(
+            "매시업3",
+            "https://i.namu.wiki/i/izVXkClWRy9-s5DAkC_lGo3za4Zy9seGH1V6AM0qZJzsckE9eWe6-Hp-1OvJm_DkVv7BL7U0Ar7QB89ApaklkQ.webp",
+            "1시간전",
+        ),
+        MockToppingGroup(
+            "매시업4",
+            "https://https://i.namu.wiki/i/h01cpKjF51MCkPZBi9-x7RMltpQbztoVgRB_0fnqrGA6S0M_9mcxpptqAmhr1YxCo0fWeErT3DGg55Nb8O2WeA.webp",
+            "1시간전",
+        ),
+    ),
     val groupAddButtonSelected: Boolean = false,
     val isTooltipVisible: Boolean = false,
     val dateString: String = "",
